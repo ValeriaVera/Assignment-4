f32e595f609196efeaf5fae6c7861ad0365f513e
ValeriaVera <Valeria@172.16.49.128>
2015-04-08 00:01:43 -0400

    initialize

---
+++ Week 12/src/Car.java
@@ -0,0 +1,47 @@
+
+public class Car {
+	private String color;
+	private double horsePower;
+	private double engineSize;
+	private String make;
+	
+	public Car(){
+		this.color= "red";
+		this.horsePower = 0;
+		this.engineSize = 0;
+		this.make = "Toyota";
+	}
+	public Car(String color, double horsePower, double engineSize,String make){
+		this.color = color;
+		this.horsePower = horsePower;
+		this.engineSize= engineSize;
+		this.make = make;
+	}
+	public String setcarColor(){
+		return color;
+		} 
+	public void getcarColor(String color){
+		this.color = color;
+		}
+	public double setcarhorsePower(){
+		return horsePower;
+		} 
+	public void getcarhorsePower(double horsePower){
+		this.horsePower = horsePower;
+		}
+	public double setcarengineSize(){
+		return engineSize;
+		} 
+	public void getcarengineSize(double engineSize){
+		this.engineSize = engineSize;
+		}
+	public String setcarmake(){
+		return make;
+		} 
+	public void getcarmake(String make){
+		this.make = make;
+		}
+	public String toString(){
+		return "Color: " + this.color + "; Horse Power: " +this.horsePower + "; Engine Size: " + this.engineSize +"; Brand: " + this.make ;
+	}
+}
---
+++ Week 12/src/CarDriver.java
@@ -0,0 +1,13 @@
+
+public class CarDriver {
+	public static void main(String[] args) {
+		Car ValeriaCar = new Car();
+		System.out.println(ValeriaCar);
+		
+		Car Car1=new Car ("yellow", 1, 1, "Honda");
+		System.out.println(Car1);
+		
+		Car Car2=new Car ("White", 2, 2, "Toyota");
+		System.out.println(Car2);
+	}
+}
